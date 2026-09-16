8cd76b429bf78a7d034a8a92af676d64d01afdab
ShriRaghavi27 <rajan.shriraghavi@gmail.com>
2026-09-16 14:53:56 +0530

    product Page class added

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/ProductPage.java
@@ -0,0 +1,20 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage {
+	
+	WebDriver driver;
+	public ProductPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(xpath = "//*[@alt='Create Product...']")
+	private WebElement createNewProductBtn;
+
+}
